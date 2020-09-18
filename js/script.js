ba4ef7164a617a395bc750ea5d320a0ced1a9551
Andrea Contestabile <andreacontest@gmail.com>
2020-09-18 17:18:49 +0200

    Aggiunta nome festività e stile css

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 $(document).ready(function() {
+
+  // Definisco due variabili relative all'anno e al mese da cui iniziare il rendering
   var startYear = 2018;
   var startMonth = 0;
 
+  // Definisco una variabile che contenga un oggetto Moment.js con la data 2018-01-01
   var momentDate = moment("2018-01-01");
 
+  // Inizializzo il template Handlebars
   var source = $("#day-template").html();
   var template = Handlebars.compile(source);
 
+  // Effettuo la chiamata Ajax verso l'api contenente le festività dell'anno 2018
   $.ajax(
     {
       "url": "https://flynn.boolean.careers/exercises/api/holidays",
+      // Come "data", passo l'anno e il mese inizializzati in principio
       "data": {
         "year": startYear,
         "month": startMonth
       },
       "method": "GET",
       "success": function(data, status) {
+        // A chiamata avvenuta, eseguo la funzione renderMonth e stampo il mese scelto
         renderMonth(data);
       },
       "error": function (error) {
@@ -24,24 +31,36 @@ $(document).ready(function() {
     }
   );
 
+  // Definisco la funzione renderMonth, per renderizzare il mese scelto
   function renderMonth(data) {
+    // Definisco una variabile che contiene il numero di giorni presenti nel mese scelto
     var daysInMonth = momentDate.daysInMonth();
+    // Effettuo un ciclo per ognuno dei giorni
     for (var i = 1; i <= daysInMonth; i++) {
+      // Modifico le chiavi del contesto per il template
       var context = {
         "day": i,
         "month": momentDate.format("MMMM")
       };
+      // Compilo il template con il context
       var html = $(template(context));
+      // Converto la data corrente nel formato base
       var currentMomentDate = moment(context.day + context.month + startYear);
       var currentMomentDateFormat = currentMomentDate.format("YYYY-MM-DD");
+      // Se l'array delle festività non è vuoto:
       if (data.response.length > 0) {
+        // Per ognuna delle festività trovate, controllo se la data corrisponde con quella corrente nel ciclo
         for (var j = 0; j < data.response.length; j++) {
           if (data.response[j].date == currentMomentDateFormat ) {
+            // Se corrisponde, aggiungo la classe ".holiday"
             $(html).addClass("holiday");
-            console.log($(html));
+            // e aggiungo uno <span> contenente il nome della festività
+            var holidayName = data.response[j].name;
+            $(html).append(" &mdash; <span>" + holidayName + "</span>");
           }
         }
       }
+      // Infine faccio l'append del template compilato e stilizzato all'interno della lista ul
       $("ul.month-display").append(html);
     }
   }
