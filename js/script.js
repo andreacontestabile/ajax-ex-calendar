249b1f20b64d7c78a2648572ba0809681a12fdfc
Andrea Contestabile <andreacontest@gmail.com>
2020-09-21 12:32:36 +0200

    Eventi click e visualizzazione mese precedente/successivo

--- js/script.js
+++ js/script.js
@@ -1,38 +1,79 @@
 $(document).ready(function() {
-
   // Definisco due variabili relative all'anno e al mese da cui iniziare il rendering
   var startYear = 2018;
   var startMonth = 0;
-
-  // Definisco una variabile che contenga un oggetto Moment.js con la data 2018-01-01
-  var momentDate = moment("2018-01-01");
 
   // Inizializzo il template Handlebars
   var source = $("#day-template").html();
   var template = Handlebars.compile(source);
 
-  // Effettuo la chiamata Ajax verso l'api contenente le festività dell'anno 2018
-  $.ajax(
-    {
-      "url": "https://flynn.boolean.careers/exercises/api/holidays",
-      // Come "data", passo l'anno e il mese inizializzati in principio
-      "data": {
-        "year": startYear,
-        "month": startMonth
-      },
-      "method": "GET",
-      "success": function(data, status) {
-        // A chiamata avvenuta, eseguo la funzione renderMonth e stampo il mese scelto
-        renderMonth(data);
-      },
-      "error": function (error) {
-        alert("Error!");
-      }
+  // Al caricamento della pagina, faccio il render del mese e delle festività
+  renderPage();
+
+  // Definisco l'evento click sul pulsante PREV
+  $("#prev").click(function() {
+    // che funziona solo se startMonth è maggiore di 0
+    if (startMonth > 0) {
+      // Seleziono il mese precedente
+      startMonth -= 1;
+      // Svuoto la pagina
+      $(".month-display").empty();
+      // Effettuo nuovamente il render
+      renderPage();
+    } else {
+      // Definisco un alert nel caso l'utente prema il pulsante PREV su Gennaio 2018
+      alert("Nessun dato precedente a questa data!");
     }
-  );
+  });
+
+  // Definisco l'evento click sul pulsante NEXT
+  $("#next").click(function() {
+    // che funziona solo se startMonth è minore di 11
+    if (startMonth < 11) {
+      // Seleziono il mese successivo
+      startMonth += 1;
+      // Svuoto la pagina
+      $(".month-display").empty();
+      // Effettuo nuovamente il render
+      renderPage();
+    } else {
+      // Definisco un alert nel caso l'utente prema il pulsante NEXT su Dicembre 2018
+      alert("Nessun dato successivo a questa data!");
+    }
+  });
+
+  function renderPage() {
+    // Effettuo la chiamata Ajax verso l'api contenente le festività dell'anno 2018
+    $.ajax(
+      {
+        "url": "https://flynn.boolean.careers/exercises/api/holidays",
+        // Come "data", passo l'anno e il mese inizializzati in principio
+        "data": {
+          "year": startYear,
+          "month": startMonth
+        },
+        "method": "GET",
+        "success": function(data, status) {
+          // A chiamata avvenuta, eseguo la funzione renderMonth e stampo il mese scelto
+          renderMonth(data);
+        },
+        "error": function (error) {
+          alert("Error!");
+        }
+      }
+    );
+  }
 
   // Definisco la funzione renderMonth, per renderizzare il mese scelto
   function renderMonth(data) {
+
+    var momentObject = {
+      "year": startYear,
+      "month": startMonth
+    };
+
+    var momentDate = moment(momentObject);
+    $("h1").text(momentDate.format("MMMM YYYY"));
     // Definisco una variabile che contiene il numero di giorni presenti nel mese scelto
     var daysInMonth = momentDate.daysInMonth();
     // Effettuo un ciclo per ognuno dei giorni
@@ -45,7 +86,7 @@ $(document).ready(function() {
       // Compilo il template con il context
       var html = $(template(context));
       // Converto la data corrente nel formato base
-      var currentMomentDate = moment(context.day + context.month + startYear);
+      var currentMomentDate = moment(context.day + context.month + momentObject.year);
       var currentMomentDateFormat = currentMomentDate.format("YYYY-MM-DD");
       // Se l'array delle festività non è vuoto:
       if (data.response.length > 0) {
